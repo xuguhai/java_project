8855f68d73fe19d5f601fdbd29d1f2e365e0132c
xuguhai <3370025934@qq.com>
2024-12-23 00:13:13 +0800

    first

---
+++ Test.java
@@ -0,0 +1,10 @@
+class Test{
+	public static void main(String[] largs){
+	int a=6;
+	int b=9;
+	b=89;
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(a+b);
+	}
+}
---
+++ hello.java
@@ -0,0 +1,17 @@
+//这是 java 的快速入门， 演示 java 的开发步骤
+//对代码的相关说明
+//1. public class Hello 表示 Hello 是一个类,是一个 public 公有的类
+//2. Hello{ } 表示一个类的开始和结束
+//3. public static void main(String[] args) 表示一个主方法,即我们程序的入口
+//4. main() {} 表示方法的开始和结束
+//5. System.out.println("hello,world~"); 表示输出"hello,world~"到屏幕
+//6. ;表示语句结束
+
+public class hello {
+//编写一个 main 方法
+
+	public static void main(String[] args) {
+	System.out.println("韩顺平教育 hello");
+		}
+}
+//一个源文件中最多
