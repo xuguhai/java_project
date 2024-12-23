77aa2c16521af56ba2f4cbb881dd138b93fe491f
xuguhai <3370025934@qq.com>
2024-12-23 22:56:51 +0800

    two_day

---
+++ bianliang.java
@@ -0,0 +1,13 @@
+public class bianliang{
+	public static void main (String[] args){
+		char name ='男';
+		int age = 18;
+		int high = 180;
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(high);
+		System.out.println(high +  666 + "哇 so high");
+		System.out.println(high + " 哇 so high" + 666);
+
+	}
+}
---
+++ bijiao.java
@@ -0,0 +1,21 @@
+public class bijiao{
+	public static void main(String[] args) {
+		double a = 2.7;
+		double b = 8.1 / 3;
+		if (a == b) {
+			System.out.println("ture\n");
+		}else {
+			System.out.println("false\n");
+		}
+		//这段代码最后会输出false，原因是8.1 / 3 在计算机中其存在误差 其实际值是几乎是2.7的小数但是还是比2.7小
+		//所以在小数的比较中要考虑误差的问题，往往通过误差精度的限制来锁定
+		System.out.println(b);
+		if (a - b < 0.00001){
+			System.out.println("\nture");
+		}else{
+			System.out.println("\nfalse");
+
+		}
+	}
+
+}
